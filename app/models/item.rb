2e649b988dea314ffc80edf3ec78e17dc3185c8e
daisuke-w <w.daisuke.001@gmail.com>
2024-06-11 14:17:39 +0900

    アソシエーション、バリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
-class Item < ApplicationRecord  
+class Item < ApplicationRecord
+  # アソシエーション
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_duration
+  has_one_attached :image
+
+  # バリデーション
+  with_options presence: true do
+    validates :category_id,           numericality: { other_than: 1 }
+    validates :status_id,             numericality: { other_than: 1 }
+    validates :shipping_cost_id,      numericality: { other_than: 1 }
+    validates :prefecture_id,         numericality: { other_than: 1 }
+    validates :shipping_duration_id,  numericality: { other_than: 1 }
+    validates :name
+    validates :price,                 numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :description
+  end
 end
