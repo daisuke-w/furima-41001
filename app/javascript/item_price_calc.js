c47220dbae37664a6c0ed015bad85fba67b98049
daisuke-w <w.daisuke.001@gmail.com>
2024-06-11 15:39:24 +0900

    手数料、利益計算

---
+++ app/javascript/item_price_calc.js
@@ -0,0 +1,17 @@
+function priceCalc() {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  itemPrice.addEventListener("input", () => {
+    const inputValue = itemPrice.value;
+    const tax = 0.1;
+    const fee = Math.floor(inputValue * tax);
+
+    addTaxPrice.innerText = fee;
+    profit.innerText = (inputValue - fee);
+
+  });
+};
+
+window.addEventListener('load', priceCalc);
