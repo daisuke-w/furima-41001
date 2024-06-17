07321f34bd0085d04e5c90b2d2126007fe57d6a9
daisuke-w <w.daisuke.001@gmail.com>
2024-06-17 11:29:01 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,59 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_top, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :prefecture_id,
+      :postal_code,
+      :city,
+      :block,
+      :building,
+      :phone
+    ).merge(
+      user_id: current_user.id,
+      item_id: params[:item_id],
+      token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_top
+    # ログインユーザーと商品出品者が別人 かつ 未購入の場合以外はTopページに遷移する
+    return if (current_user.id != @item.user_id) && @item.order.blank?
+
+    redirect_to root_path
   end
 end
